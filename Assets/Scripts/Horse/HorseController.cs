4d57d30d3228cf5ecff5094f30729c38bd8c0da3
Metnias <40355580+metnias@users.noreply.github.com>
2023-06-26 12:46:55 +0900

    Bound horse rope to hands

--- Assets/Scripts/Horse/HorseController.cs
+++ Assets/Scripts/Horse/HorseController.cs
@@ -7,6 +7,9 @@ public class HorseController : MonoBehaviour
     [SerializeField]
     private Transform playerSnap = null;
 
+    [SerializeField]
+    private Transform[] ropeHinges = new Transform[2];
+
     /// <summary>
     /// 플레이어가 탈 수 있는지
     /// </summary>
@@ -30,18 +33,28 @@ public class HorseController : MonoBehaviour
 
     private void Update()
     {
-        if (!isPlayerRiding) return;
-        
+        if (!isPlayerRiding || !playerOrigin || !playerAction) return;
+
+        Transform lHand = playerAction.directInteractors[0].transform;
+        Transform rHand = playerAction.directInteractors[1].transform;
+
+        ropeHinges[0].position = lHand.position;
+        ropeHinges[1].position = rHand.position;
+
     }
 
+    #region XREvents
     public void OnPlayerRideRequest()
     {
         if (!playerRidable || isPlayerRiding) return;
+        // 플레이어 추적
         playerOrigin = PlayerManager.InstanceOrigin();
         playerAction = PlayerManager.Action();
+        // 플레이어 이동 중지
         playerOrigin.GetComponent<DynamicMoveProvider>().enabled = false;
         playerOrigin.GetComponent<ActionBasedSnapTurnProvider>().enabled = false;
         playerOrigin.GetComponent<ActionBasedContinuousTurnProvider>().enabled = false;
+        // 플레이어 말 위로 고정
         playerOrigin.SetParent(playerSnap);
         playerOrigin.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
         isPlayerRiding = true;
@@ -50,14 +63,20 @@ public class HorseController : MonoBehaviour
     public void OnPlayerLeaveRequest()
     {
         if (!isPlayerRiding) return;
+        // 플레이어 말 오른쪽으로 이동
         playerOrigin.transform.position = playerSnap.position + playerSnap.right * 2f - playerSnap.up;
+        // 플레이어 이동 허용
         playerOrigin.GetComponent<DynamicMoveProvider>().enabled = true;
         playerOrigin.GetComponent<ActionBasedSnapTurnProvider>().enabled = true;
         playerOrigin.GetComponent<ActionBasedContinuousTurnProvider>().enabled = true;
+        // 플레이어 추적 중단
         playerOrigin.SetParent(PlayerManager.Instance().transform);
         playerOrigin = null;
         playerAction = null;
         isPlayerRiding = false;
+        // 로프 리셋
+        ropeHinges[0].localPosition = new Vector3(-0.4f, 1.6f, -0.14f);
+        ropeHinges[1].localPosition = new Vector3(-0.4f, 1.6f, 0.14f);
     }
-
+    #endregion XREvents
 }
