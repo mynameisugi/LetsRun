10894c18ecf1441f874f885c8021b87fce85dd5c
castleming <psmlotte@gmail.com>
2023-07-03 17:36:15 +0900

    RankingTestPrefab 수정

--- Assets/Scripts/Display/RankingUpdate.cs
+++ Assets/Scripts/Display/RankingUpdate.cs
@@ -5,17 +5,18 @@ using UnityEngine.UI;
 
 public class RankingUpdate : MonoBehaviour
 {
-    public List<RunnerTracker> runners;
-    public Text rankingText;
+    public List<RunnerTracker> runners; // RunnerTracker 객체를 담는 리스트
+    public Text rankingText; // 랭킹을 표시할 텍스트
 
-    private StringBuilder rankingBuilder = new StringBuilder();
+    private StringBuilder rankingBuilder = new StringBuilder(); // 랭킹 텍스트를 구성하기 위한 StringBuilder 객체
 
     private void Update()
     {
+        // 삽입 정렬을 사용하여 랭킹을 업데이트
         for (int i = 1; i < runners.Count; i++)
         {
-            RunnerTracker currentRunner = runners[i];
-            float currentScore = currentRunner.GetRankingScore();
+            RunnerTracker currentRunner = runners[i]; // 현재 RunnerTracker 객체
+            float currentScore = currentRunner.GetRankingScore(); // 현재 랭킹 점수
             int j = i - 1;
 
             while (j >= 0 && runners[j].GetRankingScore() < currentScore)
@@ -27,23 +28,22 @@ public class RankingUpdate : MonoBehaviour
             runners[j + 1] = currentRunner;
         }
 
-        UpdateRankingText();
+        UpdateRankingText(); // 랭킹 텍스트 업데이트
     }
 
     private void UpdateRankingText()
     {
-        rankingBuilder.Clear();
+        rankingBuilder.Clear(); // StringBuilder 초기화
 
+        // 모든 러너에 대해 랭킹 텍스트 구성
         for (int i = 0; i < runners.Count; i++)
         {
-            RunnerTracker runner = runners[i];
-            float rankingScore = runner.GetRankingScore();
-            float lapTime = runner.GetLapTime();
+            RunnerTracker runner = runners[i]; // 러너 객체
+            float lapTime = runner.GetLapTime(); // 주행 시간
 
             rankingBuilder.Append($"Rank {i + 1}: {runner.name}           (Time: {lapTime.ToString("F2")}s)\n");
         }
 
-        rankingText.text = rankingBuilder.ToString();
-        rankingText.gameObject.SetActive(true);
+        rankingText.text = rankingBuilder.ToString(); // 텍스트 업데이트
     }
 }
