bbe8a02d603fc75118b20329e3ef37b992834ce9
Metnias <40355580+metnias@users.noreply.github.com>
2023-07-05 11:14:06 +0900

    WIP clock time skip

--- Assets/Scripts/Watch/HWClock.cs
+++ Assets/Scripts/Watch/HWClock.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class HWClock : MonoBehaviour
 {
@@ -10,6 +9,9 @@ public class HWClock : MonoBehaviour
 
     [SerializeField]
     private RectTransform arm;
+
+    [SerializeField]
+    private HWPopup popup;
 
     private TimeManager time;
 
@@ -21,8 +23,50 @@ public class HWClock : MonoBehaviour
     private void Update()
     {
         float t = TimeManager.LOOP - time.Now;
+        float rot = t * 360f / TimeManager.LOOP;
+        if (dragMode > 0)
+        {
+            if (dragMode == 1)
+            {
+                var dir = arm.parent.InverseTransformDirection(dragger.position - arm.position);
+                dragAngle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f;
+                if (dragAngle < 0f) dragAngle += 360f;
+                dragAngle = Mathf.Clamp(dragAngle, SKIPLIMIT * 360f / TimeManager.LOOP, rot); // 10초 제한
+                arm.localRotation = Quaternion.Euler(0f, 0f, dragAngle);
+            }
+            return;
+        }
         int ti = Mathf.CeilToInt(t);
         textTime.text = $"{ti / 60:0}:{ti % 60:00}";
-        arm.localRotation = Quaternion.Euler(0f, 0f, t * 360f / TimeManager.LOOP);
+        arm.localRotation = Quaternion.Euler(0f, 0f, rot);
+    }
+
+    private const int SKIPLIMIT = 10;
+    private float dragAngle;
+    private Transform dragger = null;
+    private byte dragMode = 0;
+
+    public void OnArmDragStart()
+    {
+        if (TimeManager.LOOP - time.Now < SKIPLIMIT) return; // 스킵 불가
+        dragMode = 1;
+        Time.timeScale = 0f;
+        var pokers = PlayerManager.Instance().GetComponentsInChildren<XRPokeInteractor>();
+        dragger = pokers[1].attachTransform;
+    }
+
+    public void OnArmDragEnd()
+    {
+        dragMode = 2;
+        int offset = Mathf.FloorToInt(dragAngle / 360f * TimeManager.LOOP - (TimeManager.LOOP - time.Now));
+        if (offset < 10) { dragMode = 0; return; }
+        string offsetText = "";
+        if (offset > 60) offsetText = $"{offset / 60}분 ";
+        if (offset % 60 > 0) offsetText += $"{offset % 60}초";
+
+        popup.OpenPopup("시간을 넘기겠습니까?",
+            $"{offsetText} 뒤로\r\n시간을 넘깁니다.",
+            () => { Time.timeScale = 1f; dragMode = 0; },
+            () => { Time.timeScale = 1f; dragMode = 0; });
     }
 }
