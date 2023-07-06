d443636c22e9b781f3d8fef82320526b2d66c76e
Metnias <40355580+metnias@users.noreply.github.com>
2023-07-06 15:04:58 +0900

    Added idle race camera behaviour

--- Assets/Scripts/Display/RaceCamera.cs
+++ Assets/Scripts/Display/RaceCamera.cs
@@ -4,41 +4,29 @@ public class RaceCamera : MonoBehaviour
 {
     private float focusTime = 1f;
     private int focusEntry = 0;
+    private Vector3 focusAt = Vector3.forward;
 
     private void Update()
     {
         var race = GameManager.Instance().Race.CurrentRace;
-        if (!race)
-        {
-            return;
-        }
         focusTime -= Time.deltaTime;
-        if (focusTime <= 0f) RerollFocus();
-        var entry = race.GetEntry(focusEntry);
-        if (!entry) { RerollFocus(); return; }
-        Vector3 focus = entry.transform.position;
+        if (focusTime <= 0f) RerollFocus(race);
+        if (race)
+        {
+            var entry = race.GetEntry(focusEntry);
+            if (!entry) { RerollFocus(race); return; }
+            focusAt = entry.transform.position;
+        }
         Quaternion lastRot = transform.rotation;
-        transform.LookAt(focus, Vector3.up);
+        transform.LookAt(focusAt, Vector3.up);
         transform.rotation = Quaternion.Slerp(lastRot, transform.rotation, Time.deltaTime * 2f);
 
-        void RerollFocus()
+        void RerollFocus(bool race)
         {
-            focusEntry = Random.Range(0, 8);
             focusTime = Random.Range(1f, 4f);
+            if (!race) focusTime *= 4f;
+            if (race) focusEntry = Random.Range(0, 8);
+            else focusAt = transform.position + new Vector3(Random.value - 0.5f, -Random.value, Random.value - 0.5f).normalized;
         }
-
-        /*
-        static Vector3 GetFocus(Race curRace)
-        {
-            List<Transform> entries = new();
-            for (int i = 0; i < 8; ++i)
-            {
-                var entry = curRace.GetEntry(i);
-                if (entry) entries.Add(entry.transform);
-            }
-            Vector3 focus = Vector3.zero;
-            foreach (var tf in entries) focus += tf.position;
-            return focus / entries.Count;
-        } */
     }
 }
