4fed2c0a19262b20b4817e8f84bf3e3ae24dca4b
Metnias <40355580+metnias@users.noreply.github.com>
2023-07-10 15:09:19 +0900

    Added Race 1st player goal effect

--- Assets/Scripts/Racing/StartCubicle.cs
+++ Assets/Scripts/Racing/StartCubicle.cs
@@ -6,22 +6,23 @@ public class StartCubicle : MonoBehaviour
     private Transform[] starts = new Transform[8];
 
     [SerializeField]
+    private Animator cubicleAnim;
+
+    [SerializeField]
     private GameObject startBlock;
 
     public Transform GetStartPos(int i) => starts[i];
 
-    private void Start()
+    public void OpenGate()
     {
-        GameManager.Instance().Time.RegisterEvent(TimeManager.LOOP, () =>
-        {
-            GetComponent<Animator>().SetBool("Open", true);
-            startBlock.SetActive(false);
-        });
-        GameManager.Instance().Time.RegisterEvent(TimeManager.LOOP - 80, () =>
-        {
-            if (!gameObject.activeSelf) return;
-            GetComponent<Animator>().SetBool("Open", false);
-            startBlock.SetActive(true);
-        });
+        cubicleAnim.SetBool("Open", true);
+        startBlock.SetActive(false);
+    }
+
+    public void CloseGate()
+    {
+        if (!gameObject.activeSelf) return;
+        cubicleAnim.SetBool("Open", false);
+        startBlock.SetActive(true);
     }
 }
