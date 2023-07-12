595fe2627d31de3c0e5b06a88518ab90183597ec
Metnias <40355580+metnias@users.noreply.github.com>
2023-07-12 16:32:22 +0900

    Added dialogues to FoodBuy

--- Assets/Scripts/Shop/ShopManagerScript.cs
+++ Assets/Scripts/Shop/ShopManagerScript.cs
@@ -3,12 +3,19 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class ShopManagerScript : MonoBehaviour
 {
+    [Header("Goods")]
     [SerializeField]
     private int price = 1;
     [SerializeField]
     private GameObject itemPrefab = null;
     [SerializeField]
     private Transform itemSpawn = null;
+
+    [Header("NPC")]
+    [SerializeField]
+    private TextReader NPC = null;
+    [SerializeField]
+    private string purchaseDialogue = "FoodBuyMultteock";
 
     private IXRHoverInteractor interactor = null;
 
@@ -50,10 +57,15 @@ public class ShopManagerScript : MonoBehaviour
             PlayerManager.Instance().Action().GetDevice(0).SendHapticImpulse(0, 0.2f, 0.5f);
         
         var player = PlayerManager.Instance();
-        if (!player.Inventory().TryReduceMoney(price)) return; // µ∑ ∫Œ¡∑
+        if (!player.Inventory().TryReduceMoney(price)) // µ∑ ∫Œ¡∑
+        {
+            if (NPC) NPC.PlayConversation("FoodBuyFail");
+            return;
+        }
 
         var item = Instantiate(itemPrefab); // π∞∞«¿ª ªı∑Œ ∏∏µÈæÓº≠ ¡„æÓ¡‹
         item.transform.SetPositionAndRotation(itemSpawn.position, itemSpawn.rotation);
+        if (NPC) NPC.PlayConversation(purchaseDialogue);
         //var itemGrab = item.GetComponent<XRGrabInteractable>();
         //itemGrab.interactionManager.SelectEnter(pile.firstInteractorSelecting, itemGrab);
     }
