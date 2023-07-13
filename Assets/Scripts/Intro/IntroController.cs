3fd88c5c367c097f0103e50afd8276f467891242
castleming <psmlotte@gmail.com>
2023-07-13 15:20:35 +0900

    intro 말 생성(진행중)

--- Assets/Scripts/Intro/IntroController.cs
+++ Assets/Scripts/Intro/IntroController.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class IntroController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private GameObject horsePrefab;
+
+    [SerializeField]
+    private Transform horseSpawn;
+
+    private enum Status
     {
-        
+        Horseselect,
+        Obstacletutorial,
+        Endtutorial
     }
 
-    // Update is called once per frame
-    void Update()
+    private Status curStatus = Status.Horseselect;
+
+    private void Start()
     {
+        // 말 생성
+        for(int i = 0; i< 10; i++) 
+        {
+            var horseObject = Instantiate(horsePrefab);
+
+            Vector3 randomPosition = new Vector3(Random.Range(-10f, 10f), 0f, Random.Range(-10f, 10f));
+            horseObject.transform.position = randomPosition;
+
+            Quaternion randomRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f);
+            horseObject.transform.rotation = randomRotation;    
+        }
+
+        // 장애물 생성
+
         
+        // 지하도 막기
+    }
+
+    private void Update()
+    {
+      
     }
 }
