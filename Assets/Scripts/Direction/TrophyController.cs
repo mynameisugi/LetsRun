4b8a8843d83ad726a2b7527adcff66e084ae09e7
castleming <psmlotte@gmail.com>
2023-07-16 22:52:10 +0900

    오프닝

--- Assets/Scripts/Direction/TrophyController.cs
+++ Assets/Scripts/Direction/TrophyController.cs
@@ -4,18 +4,23 @@ using UnityEngine.UI;
 
 public class TrophyController : MonoBehaviour
 {
-    [SerializeField] private float rotationSpeed = 50f;
-    [SerializeField] private float scaleSpeed = 0.1f;
-    [SerializeField] private float maxScale = 3f;
-    [SerializeField] private float rotationTransitionSpeed = 2f;
+    [SerializeField] private float rotationSpeed = 50f; // 회전 속도
+    [SerializeField] private float scaleSpeed = 0.1f; // 크기 변경 속도
+    [SerializeField] private float maxScale = 3f; // 최대 크기
+    [SerializeField] private float rotationTransitionSpeed = 2f; // 회전 전환 속도
 
-    [SerializeField] private Text gameTypeText;
-    [SerializeField] private Text matchInfoText;
-    [SerializeField] private string gameType = "OOO배 게임 개최";
-    [SerializeField] private string matchInfo = "10 미터 경기";
+    [SerializeField] private Text gameTypeText; // 게임 유형을 표시하는 Text UI
+    [SerializeField] private Text matchInfoText; // 매치 정보를 표시하는 Text UI
+    [SerializeField] private string gameType; // 게임 유형
+    [SerializeField] private string matchInfo; // 매치 정보
 
-    [SerializeField] private float initialTextDelay = 8f;
-    [SerializeField] private float textCharacterDelay = 0.3f;
+    [SerializeField] private float textCharacterDelay = 0.3f; // 한 글자씩 텍스트를 표시하는 딜레이 시간
+
+    [SerializeField] private ParticleSystem fireworksParticleSystem; // 불꽃놀이 효과를 재생하는 파티클 시스템
+    [SerializeField] private float fireworksDuration = 10f; // 불꽃놀이 효과 지속 시간
+
+    [SerializeField] private CanvasGroup canvasGroup; // 페이드아웃 효과를 적용할 캔버스의 CanvasGroup 컴포넌트
+    [SerializeField] private float fadeOutDuration = 2f; // 페이드아웃 지속 시간
 
     private RectTransform rectTransform;
     private bool shouldRotateAndScale = true;
@@ -31,14 +36,11 @@ public class TrophyController : MonoBehaviour
     {
         if (shouldRotateAndScale)
         {
-            // Rotate the image around the Y-axis
-            rectTransform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+            rectTransform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime); // Y축을 기준으로 이미지 회전
 
-            // Increase the scale of the image gradually
-            Vector3 newScale = rectTransform.localScale + Vector3.one * scaleSpeed * Time.deltaTime;
+            Vector3 newScale = rectTransform.localScale + Vector3.one * scaleSpeed * Time.deltaTime; // 크기를 점차 증가
             rectTransform.localScale = newScale;
 
-            // Check if the scale has reached the maxScale
             if (newScale.x >= maxScale)
             {
                 shouldRotateAndScale = false;
@@ -55,28 +57,56 @@ public class TrophyController : MonoBehaviour
         while (elapsedTime < 1f)
         {
             elapsedTime += Time.deltaTime * rotationTransitionSpeed;
-            rectTransform.rotation = Quaternion.Lerp(startRotation, targetRotation, elapsedTime);
+            rectTransform.rotation = Quaternion.Lerp(startRotation, targetRotation, elapsedTime); // 회전 전환
             yield return null;
         }
 
         rectTransform.rotation = targetRotation;
+
+        StartCoroutine(ShowTextOneByOne(gameType, gameTypeText, textCharacterDelay));
+        StartCoroutine(ShowTextOneByOne(matchInfo, matchInfoText, textCharacterDelay));
     }
 
-    private void Start()
+    private IEnumerator ShowTextOneByOne(string text, Text textComponent, float characterDelay)
     {
-        StartCoroutine(ShowTextOneByOne(gameType, gameTypeText, initialTextDelay, textCharacterDelay));
-        StartCoroutine(ShowTextOneByOne(matchInfo, matchInfoText, initialTextDelay, textCharacterDelay));
-    }
-
-    private IEnumerator ShowTextOneByOne(string text, Text textComponent, float initialDelay, float characterDelay)
-    {
-        yield return new WaitForSeconds(initialDelay);
-
         textComponent.text = "";
         foreach (char c in text)
         {
-            textComponent.text += c;
+            textComponent.text += c; // 한 글자씩 텍스트에 추가
             yield return new WaitForSeconds(characterDelay);
         }
+
+        PlayFireworks();
+    }
+
+    private void PlayFireworks()
+    {
+        fireworksParticleSystem.Play(); // 불꽃놀이 효과 재생
+
+        StartCoroutine(StopFireworks());
+    }
+
+    private IEnumerator StopFireworks()
+    {
+        yield return new WaitForSeconds(fireworksDuration);
+        fireworksParticleSystem.Stop(); // 일정 시간 후 불꽃놀이 효과 정지
+
+        StartCoroutine(FadeOutCanvas());
+    }
+
+    private IEnumerator FadeOutCanvas()
+    {
+        float elapsedTime = 0f;
+        float startAlpha = canvasGroup.alpha;
+
+        while (elapsedTime < fadeOutDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / fadeOutDuration;
+            canvasGroup.alpha = Mathf.Lerp(startAlpha, 0f, t); // 캔버스 페이드아웃
+            yield return null;
+        }
+
+        canvasGroup.alpha = 0f;
     }
 }
